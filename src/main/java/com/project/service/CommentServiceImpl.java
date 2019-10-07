bb61b980196035d2436865ece887ec49f9da3ac5
BenKats <katsbenjamin@gmail.com>
2019-10-07 09:45:14 -0400

    Added @Service Bean

--- src/main/java/com/project/service/CommentServiceImpl.java
+++ src/main/java/com/project/service/CommentServiceImpl.java
@@ -2,7 +2,9 @@ package com.project.service;
 
 import com.project.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CommentServiceImpl implements CommentService {
     @Autowired
     CommentRepository commentRepository;
