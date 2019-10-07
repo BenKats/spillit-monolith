95a5b9227a10a6c98ab453557b6ecf520b741acd
BenKats <katsbenjamin@gmail.com>
2019-10-07 09:26:31 -0400

    Created User Service and its implement

---
+++ src/main/java/com/project/service/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.project.service;
+
+import com.project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserRepository userRepository;
+}
