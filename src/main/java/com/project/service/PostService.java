0c23e4b482c0c697c1b436034317135df5d55e2b
EngineerMel <ramelhaines25@gmail.com>
2019-10-07 15:41:17 -0400

    added post endpoints

--- src/main/java/com/project/service/PostService.java
+++ src/main/java/com/project/service/PostService.java
@@ -1,4 +1,15 @@
 package com.project.service;
 
+import com.project.model.Post;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
 public interface PostService {
+
+    Post createPost(Post newPost);
+
+    List<Post> listPosts();
+
+    HttpStatus deleteById(Long postId);
 }
--- src/main/java/com/project/service/PostServiceImpl.java
+++ src/main/java/com/project/service/PostServiceImpl.java
@@ -1,11 +1,34 @@
 package com.project.service;
 
+import com.project.model.Post;
 import com.project.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostServiceImpl implements PostService {
+
     @Autowired
     PostRepository postRepository;
+
+    @Override
+    public Post createPost(Post newPost){
+        return postRepository.save(newPost);
+    }
+
+    @Override
+    public List<Post> listPosts(){
+        return postRepository.findAll();
+    }
+
+    @Override
+    public HttpStatus deleteById(Long postId){
+        postRepository.deleteById(postId);
+        return HttpStatus.OK;
+    }
+
 }
+
