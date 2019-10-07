6f6a82e235a5c9d3b48778df483fd7e88beba99f
BenKats <katsbenjamin@gmail.com>
2019-10-07 09:41:46 -0400

    Created User Controller

---
+++ src/main/java/com/project/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.project.controller;
+
+import com.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+}
