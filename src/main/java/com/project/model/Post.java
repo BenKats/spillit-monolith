9291b085249a9ec99917aefe949d0406fa4a3a5e
BenKats <katsbenjamin@gmail.com>
2019-10-06 23:01:52 -0400

    Created Post table, no relational mapping

--- src/main/java/com/project/model/Post.java
+++ src/main/java/com/project/model/Post.java
@@ -1,4 +1,42 @@
 package com.project.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    public Post() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
