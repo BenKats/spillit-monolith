4951e1b429e4060a44b5af79afd2bb2dfbb20929
BenKats <katsbenjamin@gmail.com>
2019-10-08 10:58:44 -0400

    Created list all comments and list comments by postId endpoints, also changed naming of listCommentsOfUser to avoid ambiguous method error

--- src/main/java/com/project/repository/CommentRepository.java
+++ src/main/java/com/project/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.project.repository;
 
 import com.project.model.Comment;
+import com.project.model.Post;
 import com.project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,8 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
     List<Comment> findCommentsByUser(User user);
+
+    List<Comment> findCommentsByPost(Post post);
 
 }
