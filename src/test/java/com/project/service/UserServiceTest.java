8418212a6788d32c5fc0153ea6e872057a098d5a
EngineerMel <ramelhaines25@gmail.com>
2019-10-11 10:26:05 -0400

    fixed UserProfileControllerTest errors

--- src/test/java/com/project/service/UserServiceTest.java
+++ src/test/java/com/project/service/UserServiceTest.java
@@ -1,75 +1,79 @@
-//package com.project.service;
-//
-//import com.project.config.JwtUtil;
-//import com.project.model.User;
-//import com.project.repository.UserProfileRepository;
-//import com.project.repository.UserRepository;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnitRunner;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.mockito.ArgumentMatchers.anyString;
-//import static org.mockito.Mockito.when;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class UserServiceTest {
-//
-//    @Mock
-//    UserRepository userRepository;
-//
-//    @Mock
-//    private UserProfileService userProfileService;
-//
-//    @Mock
-//    private UserProfileRepository userProfileRepository;
-//
-//    @Mock
-//    private JwtUtil jwtUtil;
-//
-//    @Mock
-//    private PasswordEncoder passwordEncoder;
-//
-//    @InjectMocks
-//    private UserServiceImpl userService;
-//
-//
-//    @InjectMocks
-//    private User user;
-//
-//    @Before
-//    public void initializeDummyUser(){
-//        user.setUsername("test1");
-//        user.setPassword("test1");
-//    }
-//
-//    @Test
-//    public void getUser_ReturnsUser_Success(){
-//
-//        user.setUsername("test1");
-//        user.setPassword("test1");
-//
-//        when(userRepository.findByUsername(anyString())).thenReturn(user);
-//
-//        User tempUser = userService.getUser(user.getUsername());
-//
-//        assertEquals(tempUser.getUsername(), user.getUsername());
-//    }
+package com.project.service;
+
+import com.project.config.JwtUtil;
+import com.project.model.User;
+import com.project.repository.UserProfileRepository;
+import com.project.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    private UserProfileService userProfileService;
+
+    @Mock
+    private UserProfileRepository userProfileRepository;
+
+
+    @Mock
+    private JwtUtil jwtUtil;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+
+    @InjectMocks
+    private User user;
+
+    @Before
+    public void initializeDummyUser(){
+        user.setUsername("test1");
+        user.setPassword("test1");
+    }
+
+    @Test
+    public void getUser_ReturnsUser_Success(){
+
+        user.setUsername("test1");
+        user.setPassword("test1");
+
+        when(userRepository.findByUsername(anyString())).thenReturn(user);
+
+        User tempUser = userService.getUser(user.getUsername());
+
+        assertEquals(tempUser.getUsername(), user.getUsername());
+    }
+
+//            have to fix login(user)
 //
 //    @Test
 //    public void login_UserNotFound_Error(){
 //        user.setUsername("test1");
 //        user.setPassword("test1");
+//       when(userRepository.findByUsername(anyString())).thenReturn(null);
+//       String token = userService.login(user);
+//      assertEquals(token, null);
 //
-//        when(userRepository.findByUsername(anyString())).thenReturn(null);
-//
-//        String token = userService.login(user);
-//
-//        assertEquals(token, null);
 //    }
-//
-//}
+
+}
