63a0ea5fdfe7a05e63c11fee9d8402603c31f555
BenKats <katsbenjamin@gmail.com>
2019-10-13 15:16:26 -0400

    Alternative way to enable CORS, currently unused

---
+++ src/main/java/com/project/config/MyCorsFilter.java
@@ -0,0 +1,31 @@
+//package com.project.config;
+//
+//import org.apache.catalina.filters.CorsFilter;
+//import org.springframework.stereotype.Component;
+//
+//import javax.servlet.Filter;
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.ServletRequest;
+//import javax.servlet.ServletResponse;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//
+//@Component
+//public class MyCorsFilter extends CorsFilter {
+//
+//    @Override
+//    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+//        HttpServletResponse response = (HttpServletResponse) res;
+//        HttpServletRequest request = (HttpServletRequest) req;
+//
+//        response.setHeader("Access-Control-Allow-Credentials", "true");
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With, Origin");
+//        chain.doFilter(req, res);
+//    }
+//}
