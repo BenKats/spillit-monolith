15d6d18263adbf21a3be0320402635658a958b0a
BenKats <katsbenjamin@gmail.com>
2019-10-14 02:59:45 -0400

    Simplified which methods need authentication

--- src/main/java/com/project/config/SecurityConfig.java
+++ src/main/java/com/project/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,8 +37,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
 
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers().permitAll()
-                .antMatchers("/user/blah").hasRole("USER")
+                .antMatchers(HttpMethod.POST).authenticated()
+                .antMatchers(HttpMethod.DELETE).authenticated()
+                .antMatchers(HttpMethod.PUT).authenticated()
+//                .antMatchers("/user/blah").hasRole("USER")
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic()
